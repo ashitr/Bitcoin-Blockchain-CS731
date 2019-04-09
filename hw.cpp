8fe1064cbd04d19f52ddd9aad08b6fec49987956
ashitr <45786431+ashitr@users.noreply.github.com>
2019-04-09 23:39:26 +0530

    Add files via upload

---
+++ hw.cpp
@@ -0,0 +1,44 @@
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <stdio.h>
+#include <string.h>
+
+#define KEY_LENGTH  2048
+#define PUB_EXP     3
+#define PRINT_KEYS
+#define WRITE_TO_FILE
+
+int main(void) {
+    size_t pri_len;            // Length of private key
+    size_t pub_len;            // Length of public key
+    char   *pri_key;           // Private key
+    char   *pub_key;           // Public key
+    char   msg[KEY_LENGTH/8];  // Message to encrypt
+    char   *encrypt = NULL;    // Encrypted message
+    char   *decrypt = NULL;    // Decrypted message
+    char   *err;               // Buffer for any error messages
+
+    // Generate key pair
+    printf("Generating RSA (%d bits) keypair...", KEY_LENGTH);
+    fflush(stdout);
+    RSA *keypair = RSA_generate_key(KEY_LENGTH, PUB_EXP, NULL, NULL);
+
+    // To get the C-string PEM form:
+    BIO *pri = BIO_new(BIO_s_mem());
+    BIO *pub = BIO_new(BIO_s_mem());
+
+    PEM_write_bio_RSAPrivateKey(pri, keypair, NULL, NULL, 0, NULL, NULL);
+    PEM_write_bio_RSAPublicKey(pub, keypair);
+
+    free_stuff:
+    RSA_free(keypair);
+    BIO_free_all(pub);
+    BIO_free_all(pri);
+    free(pri_key);
+    free(pub_key);
+    free(encrypt);
+    free(decrypt);
+    free(err);
+
+}
